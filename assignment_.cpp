10c43d6b481c4291d45803c462166ee5be31757b
Rubel hossain <42895415+rirubelhossain@users.noreply.github.com>
2020-10-27 23:47:22 +0600

    Add files via upload

---
+++ assignment_.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+
+int main()
+{
+
+    char ch[13] ="saifulislam" ;
+    int counter = 0 ;
+    for(int i = 0 ; i < 13 ; ++i)
+    {
+        ++counter ;
+    }
+
+    cout<<counter<<endl;
+
+
+
+
+    return(0);
+}
