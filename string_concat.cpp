e521ebc49d1c9d2757bd5b8bbfb64b0c6ca76649
Rubel hossain <42895415+rirubelhossain@users.noreply.github.com>
2020-10-27 20:31:47 +0600

    Add files via upload

---
+++ string_concat.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <strings.h>
+
+using namespace std ;
+
+
+int main()
+{
+    string a , b ;
+
+    getline(cin , a );
+    getline(cin , b );
+
+    cout<<a <<' '<<b;
+
+
+    return(0);
+}
